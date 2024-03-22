97529ca098da8ba0ea77b101eedb31ced0ce548f
JoaoGomes8 <joaogomes2609@gmail.com>
2024-03-22 01:14:09 +0000

    first js functionalities

---
+++ 01_funcionalidades_js/app.js
@@ -0,0 +1,16 @@
+let nome = "joao";
+console.log(nome);
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(100, 200));
+
+// console.log(global);
+
+// setTimeout(() => {
+//    console.log('Operação concluída'); 
+// }, 3000);
+
+console.log("DIRECTORY: " + __dirname);
+console.log("FILENAME: " + __filename);
