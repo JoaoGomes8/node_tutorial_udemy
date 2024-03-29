e92195a9eab59d3e2679e9439ddda2d8db6422a9
JoaoGomes8 <joaogomes2609@gmail.com>
2024-03-29 19:26:28 +0000

    express routes

--- 12_express_1/server.js
+++ 12_express_1/server.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require("express");
 // const server = express()
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Olá ExpressJs!')
+// routes
+app.get("/", (req, res) => {
+  res.status(200).send("Olá ExpressJs!");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send({name: "joao"});
+});
+
+app.use((req, res) =>{
+    res.status(404).send('Erro!')
 })
 
-app.listen(3000)
+app.listen(3000);
