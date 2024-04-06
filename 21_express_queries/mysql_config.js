70ec9db737d885ebd5919e97f63e0e314a6d169c
JoaoGomes8 <joaogomes2609@gmail.com>
2024-04-06 23:50:25 +0100

    mysql connection

---
+++ 21_express_queries/mysql_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: "localhost",
+  user: "user_bd_tasks",
+  password: "vLjFSIfTHw33kCiV",
+  database: "nodejs_tasks",
+};
+
--- 21_express_queries/server.js
+++ 21_express_queries/server.js
@@ -1,27 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
-
+const mysql_config = require("./mysql_config");
 const app = express();
 app.listen(3000, () => {
   console.log("Servidor em execução");
 });
 
-// criar a ligação
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "user_bd_tasks",
-  password: "vLjFSIfTHw33kCiV",
-  database: "nodejs_tasks",
-});
+// mysql connection
+const connection = mysql.createConnection(mysql_config);
 
-connection.connect((error) => {
-  if (error) {
-    console.log("Erro na conexão ao MySQL: " + error.stack);
-    return;
-  }
-  console.log("Conexão com sucesso");
-});
-
+// routes
 app.get("/", (req, res) => {
   // query
   connection.query("SELECT * FROM tasks", (err, results, fields) => {
